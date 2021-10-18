3a8ff4c0a829ec8b42bf16f78e4509314382e79a
Phoenixdu <freddy.bedran@hotmail.com>
2021-10-18 18:30:13 -0400

    Camera and Player setup
    Free look camera and player moving in the direction you're looking at. Rotation applies

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam; //camera follow again, comment to disable
+
+    public float moveSpeed = 6f;
+
+    //Character roation with camera, comment to disable
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            //Rotates camera and player smoothly depending on direction faced, comment to disable
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * moveSpeed * Time.deltaTime);
+
+            //Used for regular movement with no camera follow, comment to disable
+            //controller.Move(direction * moveSpeed * Time.deltaTime);
+
+        }
+    }
+
+
+}
