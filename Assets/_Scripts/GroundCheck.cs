a940826d864e34899a5ba757fa597778f922e181
Julaxe <julaxe8@gmail.com>
2021-12-03 17:29:58 -0500

    pushable pillar prefab created with RotateWhehPushed script

--- Assets/_Scripts/GroundCheck.cs
+++ Assets/_Scripts/GroundCheck.cs
@@ -13,30 +13,5 @@ public class GroundCheck : MonoBehaviour
                     );
         }
     }
-    //private List<Collider> listOfTiles;
-
-    //private void Start()
-    //{
-    //    listOfTiles = new List<Collider>();
-    //}
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if(other.CompareTag("Tile"))
-    //    {
-    //        listOfTiles.Add(other);
-    //    }
-    //}
-    //private void OnTriggerExit(Collider other)
-    //{
-    //    if (other.CompareTag("Tile"))
-    //    {
-    //        listOfTiles.Remove(other);
-    //        if(listOfTiles.Count == 0)
-    //        {
-    //            UnityEngine.SceneManagement.SceneManager.LoadScene(
-    //                UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex
-    //                );
-    //        }
-    //    }
 }
 
---
+++ Assets/_Scripts/RotateWhenPushed.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class RotateWhenPushed : MonoBehaviour
+{
+    [SerializeField] private string tagOfPusher = "Player";
+    [SerializeField] private Transform moveTransform = null;
+    [SerializeField] private Vector3 moveOffset = Vector3.zero;
+    [SerializeField] private Transform rotateTransform = null;
+    [SerializeField] private Vector3 rotateOffset = Vector3.zero;
+    [SerializeField] private float moveDuration = 1.0f;
+    [SerializeField] private Ease easeType = Ease.OutBack;
+    [SerializeField] private bool hideOnComplete = false;
+
+    private bool activated = false;
+
+    private void Start()
+    {
+        Debug.Log("Started");
+    }
+
+    public void Activate()
+    {
+        if (moveTransform == null)
+            moveTransform = transform;
+        if (rotateTransform == null)
+            rotateTransform = transform;
+
+        if (moveOffset != Vector3.zero)
+        {
+            moveTransform.DOMove(moveTransform.position + moveOffset, moveDuration).SetEase(easeType)
+                .OnComplete(() =>
+                {
+                    if (hideOnComplete)
+                        gameObject.SetActive(false);
+                });
+        }
+
+        if (rotateOffset != Vector3.zero)
+        {
+            rotateTransform.DORotate(rotateTransform.rotation.eulerAngles + rotateOffset, moveDuration).SetEase(easeType)
+                .OnComplete(() =>
+                {
+                    if (hideOnComplete)
+                        gameObject.SetActive(false);
+                });
+        }
+        
+        activated = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!activated)
+        {
+            if (other.gameObject.CompareTag(tagOfPusher))
+            {
+                Activate();
+                Debug.Log("Colliding");
+            }
+        }
+    }
+}
