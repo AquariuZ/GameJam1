e247e37c019bbaa9072343ae8692b10ae80a2415
Russell Li <russell.li@rocketmail.com>
2022-03-23 19:56:30 -0400

    Added Speech bubble script and add it to the sacrifices prefabs
    Added Speech bubble script and add it to the sacrifices prefabs

---
+++ Assets/_Scripts/SpeechBubbles.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechBubbles : MonoBehaviour
+{
+    public string FlavorText = "Enter Text Here";
+    public GameObject Sacrifice;
+    PickupPerson pickupPerson;
+    private bool isActivated;
+
+    void Start()
+    {
+        isActivated = false;
+        pickupPerson = Sacrifice.GetComponent<PickupPerson>();
+    }
+
+    void OnGUI()
+    {
+        // Make a multiline text area that modifies stringToEdit.
+        if (isActivated)
+        {
+            FlavorText = GUI.TextArea(new Rect(300, 310, 300, 80), FlavorText, 200);
+        }
+    }
+
+    void Update()
+    {
+        if (pickupPerson.follow)
+        {
+            isActivated = true;
+        }
+    }
+}
